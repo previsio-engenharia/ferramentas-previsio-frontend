c0de5ce97cad22b8b75e95a0cc9118924ee83090
Joel De Conto <94745731+joeldc94@users.noreply.github.com>
2023-11-08 17:39:36 -0300

    manifest ok

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,7 @@ export default class CustomDocument extends Document {
           <meta name="keywords" content="Segurança do trabalho, NR" />
           <meta name="robots" content="index, follow" />
           <meta name="author" content="Joel De Conto" />
+          <link rel="manifest" href="/manifest.json" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500;600&display=swap" rel="stylesheet" />
