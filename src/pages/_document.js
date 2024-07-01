c181b202156226600827814183f1020d86fbe39a
Joel De Conto <94745731+joeldc94@users.noreply.github.com>
2024-07-01 09:45:59 -0300

    adicionado script adsense

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,25 +1,27 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class CustomDocument extends Document {
-  render() {
-    return (
-      <Html lang='pt-br'>
-        <Head>
-          <meta name="keywords" content="Segurança do trabalho, NR" />
-          <meta name="robots" content="index, follow" />
-          <meta name="author" content="Joel De Conto" />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500;600&display=swap" rel="stylesheet" />
-        </Head>
+    render() {
+        return (
+            <Html lang='pt-br'>
+                <Head>
+                    <meta name="keywords" content="Segurança do trabalho, NR" />
+                    <meta name="robots" content="index, follow" />
+                    <meta name="author" content="Joel De Conto" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500;600&display=swap" rel="stylesheet" />
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9453424231422521"
+                        crossOrigin="anonymous"></script>
+                </Head>
 
-        <body>
+                <body>
 
-          <Main />
-        </body>
-        <NextScript />
-      </Html>
-    )
-  }
+                    <Main />
+                </body>
+                <NextScript />
+            </Html>
+        )
+    }
 }
